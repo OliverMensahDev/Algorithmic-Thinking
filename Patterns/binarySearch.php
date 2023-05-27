2c3893ac94399576ea611d19dd2e341d2a54302c
Oliver Mensah <olivermensah96@gmail.com>
2023-05-27 05:32:43 +0000

    patterns

--- Patterns/binarySearch.php
+++ Patterns/binarySearch.php
@@ -35,6 +35,32 @@ echo binarySearch([10, 6, 4], 10) . PHP_EOL;
 echo binarySearch([10, 6, 4], 4) . PHP_EOL;
 
 /**
+ * Given an array of numbers sorted in ascending order, find the floor of a given number ‘key’. 
+ * The floor of the ‘key’ will be the biggest element in the given array smaller than or equal to the ‘key’
+ * Write a function to return the index of the floor of the ‘key’. If there isn’t a floor, return -1.
+ */
+function searchFloorOfANumber(array $arr, int $key){
+    if($key <  $arr[0]) return -1;
+    $n = count($arr);
+    $start = 0;
+    $end = $n -1;
+    $mids = [];
+    while($start <= $end){
+        $mid = floor($start + ($end - $start) / 2);
+        $mids[] = $mid;
+        if($key < $arr[$mid]){
+            $end = $mid - 1;
+        }else if($key > $arr[$mid]){
+            $start = $mid + 1;
+        }else{
+            return $mid;
+        }
+    }
+    print_r($mids);
+    return $end;
+}
+
+/**
  * Given an array of numbers sorted in an ascending order, find the ceiling of a given number ‘key’. 
  * The ceiling of the ‘key’ will be the smallest element in the given array greater than or equal to the ‘key’.
  * Write a function to return the index of the ceiling of the ‘key’. If there isn’t any ceiling return -1.
@@ -44,52 +70,29 @@ function searchCeilingOfANumber(array $arr, int $key){
     if($key >  $arr[$n - 1]) return -1;
     $start = 0;
     $end = $n -1;
-    $isAscending = $arr[$start] < $arr[$end];
     while($start <= $end){
         $mid = floor($start + ($end - $start) / 2);
+        if($key == $arr[$mid]) return $mid;
         if($key < $arr[$mid]){
             $end = $mid - 1;
-        }else if($key > $arr[$mid]){
-            $start = $mid + 1;
         }else{
-            return $mid;
+            $start = $mid + 1;
         }
     }
     return $start;
 }
 
-echo "searchCeilingOfANumber".  PHP_EOL;
-echo searchCeilingOfANumber([4, 6, 10], 6) . PHP_EOL;
-echo searchCeilingOfANumber([1, 3, 8, 10, 15], 12) . PHP_EOL;
-echo searchCeilingOfANumber([4, 6, 10], 17) . PHP_EOL;
-echo searchCeilingOfANumber([4, 6, 10], -1) . PHP_EOL;
 
-/**
- * Given an array of numbers sorted in ascending order, find the floor of a given number ‘key’. 
- * The floor of the ‘key’ will be the biggest element in the given array smaller than or equal to the ‘key’
- * Write a function to return the index of the floor of the ‘key’. If there isn’t a floor, return -1.
- */
-function searchFloorOfANumber(array $arr, int $key){
-    $n = count($arr);
-    if($key <  $arr[0]) return -1;
-    $start = 0;
-    $end = $n -1;
-    $isAscending = $arr[$start] < $arr[$end];
-    while($start <= $end){
-        $mid = floor($start + ($end - $start) / 2);
-        if($key < $arr[$mid]){
-            $end = $mid - 1;
-        }else if($key > $arr[$mid]){
-            $start = $mid + 1;
-        }else{
-            return $mid;
-        }
-    }
-    return $end;
-}
+$data = [1, 2, 8, 10, 12, 19, 21];
+echo "The floor of 0 is ". searchFloorOfANumber($data, 0) .  PHP_EOL;
+echo "The ceiling of 0 is ". searchCeilingOfANumber($data, 0) .  PHP_EOL;
 
-echo "searchFloorOfANumber".  PHP_EOL;
-echo searchFloorOfANumber([4, 6, 10], 6) . PHP_EOL;
-echo searchFloorOfANumber([1, 3, 8, 10, 15], 12) . PHP_EOL;
-echo searchFloorOfANumber([4, 6, 10], 17) . PHP_EOL;
-echo searchFloorOfANumber([4, 6, 10], -1) . PHP_EOL;
+echo "The floor of 1 is ". searchFloorOfANumber($data, 1) .  PHP_EOL;
+echo "The ceiling of 1 is ". searchCeilingOfANumber($data, 1) .  PHP_EOL;
+
+echo "The floor of 5 is ". searchFloorOfANumber($data, 5) .  PHP_EOL;
+echo "The ceiling of 5 is ". searchCeilingOfANumber($data, 5) .  PHP_EOL;
+
+
+echo "The floor of 20 is ". searchFloorOfANumber($data, 20) .  PHP_EOL;
+echo "The ceiling of 20 is ". searchCeilingOfANumber($data, 20) .  PHP_EOL;
