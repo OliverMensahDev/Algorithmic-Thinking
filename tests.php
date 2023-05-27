fdb1270de43f28e35389389af648c55ec0e1cdf0
Oliver Mensah <olivermensah96@gmail.com>
2023-05-27 05:33:14 +0000

    testing using binary search to find for floor and ceiling of a number

--- tests.php
+++ tests.php
@@ -1,25 +1,43 @@
 <?php
-function binarySearch(array $list, int $key){
-    $start = 0;
-    $end = count($list) - 1;
-    $isAscending = $list[0] < $list[1];
+
+function searchFloorOfANumber(array $arr, int $key){
+    if($key < $arr[0]) return -1;
+    $start = 0; 
+    $end  = count($arr) - 1; 
     while($start <= $end){
-        $mid = floor($start + ($end - $start) / 2);
-        if($key == $list[$mid]) return $mid;
-        if($isAscending){
-            if($key < $list[$mid]) $end = $mid - 1;
-            else $start = $mid + 1;
-        }else{
-            if($key < $list[$mid]) $start = $mid + 1;
-            else $end = $mid - 1;
-        }
+        $mid = floor($start +($end - $start)/2);
+        if($key < $arr[$mid])$end = $mid - 1;
+        else if($key > $arr[$mid]) $start = $mid + 1;
+        else return $mid;
     }
-    return -1; 
+
+    return $end;
 }
 
-echo binarySearch([], 1) . PHP_EOL;
-echo binarySearch([1], 1) . PHP_EOL;
-echo binarySearch([1], 2) . PHP_EOL;
-echo binarySearch([1,2,3,4], 3) . PHP_EOL;
-echo binarySearch([1,2,3,4], 0) . PHP_EOL;
-echo binarySearch([10, 9 , 8, 7], 10) . PHP_EOL;
+
+function searchCeilingOfANumber(array $arr, int $key){
+    $end = count($arr) - 1;
+    if($key > $arr[$end]) return -1;
+    $start = 0; 
+    while($start <= $end){
+        $mid = floor($start +($end - $start)/2);
+        if($key < $arr[$mid])$end = $mid - 1;
+        else if($key > $arr[$mid]) $start = $mid + 1;
+        else return $mid;
+    }
+    return $start;
+}
+
+$data = [1, 2, 8, 10, 12, 19];
+echo "The floor of 0 is ". searchFloorOfANumber($data, 0) .  PHP_EOL;
+echo "The ceiling of 0 is ". searchCeilingOfANumber($data, 0) .  PHP_EOL;
+
+echo "The floor of 1 is ". searchFloorOfANumber($data, 1) .  PHP_EOL;
+echo "The ceiling of 1 is ". searchCeilingOfANumber($data, 1) .  PHP_EOL;
+
+echo "The floor of 5 is ". searchFloorOfANumber($data, 5) .  PHP_EOL;
+echo "The ceiling of 5 is ". searchCeilingOfANumber($data, 5) .  PHP_EOL;
+
+
+echo "The floor of 20 is ". searchFloorOfANumber($data, 20) .  PHP_EOL;
+echo "The ceiling of 20 is ". searchCeilingOfANumber($data, 20) .  PHP_EOL;
