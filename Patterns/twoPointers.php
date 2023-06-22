1eebca6703d4daf1d022b248dc7279d39bf1b72b
Oliver Mensah <olivermensahdev@Olivers-Macbook-Pro.local>
2023-06-22 13:32:12 +0000

    improve code output"

--- Patterns/twoPointers.php
+++ Patterns/twoPointers.php
@@ -3,7 +3,7 @@
 function pair_with_target_sum($arr, $targetSum) {
     $left = 0;
     $right = count($arr) - 1;
-     while ($left < $right) {
+    while ($left < $right) {
          $currentSum = $arr[$left] + $arr[$right];
        if ($currentSum === $targetSum) {
          return [$left, $right];
@@ -16,11 +16,11 @@ function pair_with_target_sum($arr, $targetSum) {
        }
      }
      return [-1, -1];
-   }
+}
    
-   
- echo json_encode(pair_with_target_sum([1, 2, 3, 4, 6], 6)) . PHP_EOL;
- echo json_encode(pair_with_target_sum([2, 5, 9, 11], 11)) . PHP_EOL; 
+echo 'Pair Target Sum'. PHP_EOL;
+echo json_encode(pair_with_target_sum([1, 2, 3, 4, 6], 6)) . PHP_EOL;
+echo json_encode(pair_with_target_sum([2, 5, 9, 11], 11)) . PHP_EOL; 
  
 function removeDuplicates($arr)
 {
@@ -36,7 +36,7 @@ function removeDuplicates($arr)
     return $nextNonDuplicateIndex;
 }
 
-
+echo 'Remove duplicates'. PHP_EOL;
 echo removeDuplicates([2, 3, 3, 3, 6, 9, 9]) . PHP_EOL;
 echo removeDuplicates([2, 2, 2, 11]) . PHP_EOL;
 
@@ -55,7 +55,7 @@ function removeElement($arr, $key)
     return $nextElementIndex;
 }
 
-
+echo 'remove element'. PHP_EOL;
 echo removeElement([2, 3, 3, 3, 6, 9, 9], 3) . PHP_EOL;
 echo removeElement([2, 2, 2, 11], 2) . PHP_EOL;
 
@@ -81,7 +81,7 @@ function threeSumZero($arr) {
   return [];
 }
  
- 
+echo 'Three Sum Zero'. PHP_EOL;
 echo json_encode(threeSumZero([-2, 2, 6, -4])) . PHP_EOL;
 echo json_encode(threeSumZero([0, 0, 0])) . PHP_EOL;
 echo json_encode(threeSumZero([2, -2])) . PHP_EOL;
@@ -91,7 +91,7 @@ echo json_encode(threeSumZero([2, -2])) . PHP_EOL;
 
 
 function threeSumZero1($arr) {
-  if(count($arr) <=2) return [];
+  if(count($arr) <= 2) return [];
   sort($arr);
   for($i = 0; $i < count($arr); $i++){
     $start = $i + 1;
@@ -110,7 +110,7 @@ function threeSumZero1($arr) {
     return [];
   }
 }
-
+echo 'Three Sum Zero 1'. PHP_EOL;
 echo json_encode(threeSumZero1([-2, 2, 6, -4])) . PHP_EOL;
 echo json_encode(threeSumZero1([0, 0, 0])) . PHP_EOL;
 echo json_encode(threeSumZero1([2, -2])) . PHP_EOL;
